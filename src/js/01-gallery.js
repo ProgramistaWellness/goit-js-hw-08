fb2165a917499aaa6d51bee712f2df6e224d88cc
Oskar Przybysz <117311648+ProgramistaWellness@users.noreply.github.com>
2023-11-28 13:55:25 +0100

    Used code from homework-07 and refracted it to use import feature

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
 
+const createGallery = () =>
+  galleryItems.forEach(item => {
+    const createdListItem = document.createElement('li');
+    gallery.insertAdjacentElement('beforeend', createdListItem);
+
+    const createdLink = document.createElement('a');
+    createdListItem.insertAdjacentElement('beforeend', createdLink);
+    createdLink.href = item.original;
+    createdLink.classList.add('gallery__item');
+
+    const createdImg = document.createElement('img');
+    createdLink.insertAdjacentElement('beforeend', createdImg);
+    createdImg.src = item.preview;
+    createdImg.alt = item.description;
+    createdImg.classList.add('gallery__image');
+  });
+createGallery();
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  /* options */ captionsData: 'alt',
+  captionDelay: 250,
+});
+lightbox.on('show.simplelightbox');
 console.log(galleryItems);
