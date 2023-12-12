e5d139b3ad6f464918bb0cb1e08e4882ed3da9b2
Oskar Przybysz <117311648+ProgramistaWellness@users.noreply.github.com>
2023-12-12 12:45:17 +0100

    update video.js file

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,2 +1,17 @@
 import Player from '@vimeo/player';
-const idPlayer = new Player('vimeo-player');
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+player.on(
+  'timeupdate',
+  throttle(data => {
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+  }, 1000)
+);
+
+window.addEventListener('DOMContentLoaded', () => {
+  const time = localStorage.getItem('videoplayer-current-time');
+  player.setCurrentTime(time);
+});
